f1a5cfa39188d9dca88621f7a40332b019627708
Pavel Fokin <fokinpv@gmail.com>
2023-06-22 17:04:24 +0200

    Fix parsing basic function signature

--- internal/parser.go
+++ internal/parser.go
@@ -1,7 +1,5 @@
 package internal
 
-import "fmt"
-
 type Parser struct {
 	lexer *Lexer
 
@@ -11,11 +9,6 @@ type Parser struct {
 
 func NewParser(l *Lexer) *Parser {
 	return &Parser{lexer: l}
-}
-
-func (p *Parser) nextToken() {
-	p.curToken = p.peekToken
-	p.peekToken = p.lexer.NextToken()
 }
 
 func (p *Parser) Parse() *AST {
@@ -92,7 +85,6 @@ func (p *Parser) parseFunc() *Func {
 	}
 
 	f.Params = p.parseFuncParams()
-	p.nextToken()
 	f.Returns = p.parseFuncReturns()
 
 	return f
@@ -126,45 +118,17 @@ func (p *Parser) parseFuncParams() []string {
 func (p *Parser) parseFuncReturns() []string {
 	returns := []string{}
 
-	fmt.Println("p.curToken.Literal", p.curToken.Literal)
-	fmt.Println("p.peekToken.Literal", p.peekToken.Literal)
-
-	if !(p.peekTokenIs(tokens.IDENT) || p.peekTokenIs(tokens.LPAREN)) {
-		fmt.Println("### 0", !(p.peekTokenIs(tokens.IDENT) || p.peekTokenIs(tokens.LPAREN)))
-		return returns
-	}
-
-	fmt.Println("### 1")
-
 	if p.peekTokenIs(tokens.IDENT) {
-		fmt.Println("#", p.curToken.Literal)
-		returns = append(returns, p.curToken.Literal)
-		p.nextToken()
-		return returns
-	}
-
-	fmt.Println("### 2")
-
-	if p.peekTokenIs(tokens.RPAREN) {
-		p.nextToken()
-		return returns
-	}
-
-	p.nextToken()
-
-	returns = append(returns, p.curToken.Literal)
-
-	for p.peekTokenIs(tokens.COMMA) {
-		p.nextToken()
 		p.nextToken()
 		returns = append(returns, p.curToken.Literal)
-	}
-
-	if !p.expectPeek(tokens.RPAREN) {
-		return nil
 	}
 
 	return returns
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.lexer.NextToken()
 }
 
 func (p *Parser) peekTokenIs(tokenType TokenType) bool {
